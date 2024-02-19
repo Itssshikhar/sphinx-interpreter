a27f8ff35819a73c657bd302d1c1aa3d1b391c56
Shikhar Mishra <shikhar2807.ace@gmail.com>
2024-02-20 00:06:50 +0530

    some ast code added

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
   if err != nil {
     panic(err)
   }
-  fmt.Printf("Hello %s! This is the Sphinx programming language!\n", user.Username)
+  fmt.Printf("Aa gya Manhoos %s! This is the Sphinx programming language!\n", user.Username)
   fmt.Printf("Feel free to type in commands\n")
   repl.Start(os.Stdin, os.Stdout)
 }
