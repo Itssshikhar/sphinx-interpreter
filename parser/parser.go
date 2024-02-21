6a6bbfd081a1d83f647e34fceb9ec0cc016348b5
Shikhar Mishra <shikhar2807.ace@gmail.com>
2024-02-21 23:41:41 +0530

    more ast code added

--- parser/parser.go
+++ parser/parser.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Parser struct {
-  l *Lexer.lexer
+  l *lexer.Lexer
 
   curToken token.Token
   peekToken token.Token
@@ -29,4 +29,38 @@ func (p *Parser) nextToken() {
 
 func (p *Parser) ParseProgram() *ast.Program {
   return nil
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+  program := &ast.Program{}
+  program.Statements = []ast.Statement{}
+
+  for p.curToken.Type != token.EOF {
+    stmt := p.parseStatement()
+    if stmt != nil {
+      program.Statements = append(program.Statements, stmt)
+    }
+    p.nextToken()
+  }
+
+  return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+  switch p.curToken.Type {
+  case token.LET:
+    return p.parseLetStatement()
+  default:
+    return nil
+  }
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+  stmt := &ast.LetStatement{Token: p.curToken}
+
+  if !p.expectPeek(token.IDENT) {
+    return nil
+  }
+
+  stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
