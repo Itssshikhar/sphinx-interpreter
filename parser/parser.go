6a41e4cb28aacc75d18acc67c34f72379770bfea
Shikhar Mishra <shikhar2807.ace@gmail.com>
2024-02-29 23:54:51 +0530

    parsing return statements

--- parser/parser.go
+++ parser/parser.go
@@ -1,9 +1,10 @@
 package parser
 
 import (
-  "sphinx/ast"
-  "sphinx/lexer"
-  "sphinx/token"
+	"fmt"
+	"sphinx/ast"
+	"sphinx/lexer"
+	"sphinx/token"
 )
 
 type Parser struct {
@@ -11,15 +12,20 @@ type Parser struct {
 
   curToken token.Token
   peekToken token.Token
+  errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-  p := &Parser{l: l}
+  p := &Parser{l: l, errors: []string{},}
 
   p.nextToken()
   p.nextToken()
 
   return p
+}
+
+func (p *Parser) Errors() []string {
+  return p.errors
 }
 
 func (p *Parser) nextToken() {
@@ -28,14 +34,10 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-  return nil
-}
-
-func (p *Parser) ParseProgram() *ast.Program {
   program := &ast.Program{}
   program.Statements = []ast.Statement{}
 
-  for p.curToken.Type != token.EOF {
+  for !p.curTokenIs(token.EOF) {
     stmt := p.parseStatement()
     if stmt != nil {
       program.Statements = append(program.Statements, stmt)
@@ -50,6 +52,8 @@ func (p *Parser) parseStatement() ast.Statement {
   switch p.curToken.Type {
   case token.LET:
     return p.parseLetStatement()
+  case token.RETURN:
+    return p.parseReturnStatement()
   default:
     return nil
   }
@@ -63,4 +67,49 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
   }
 
   stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+  if !p.expectPeek(token.ASSIGN) {
+    return nil
+  }
+
+  for !p.curTokenIs(token.SEMICOLON) {
+    p.nextToken()
+  }
+
+  return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+  return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+  return p.peekToken.Type == t
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+  msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+  p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+  if p.peekTokenIs(t) {
+    p.nextToken()
+    return true
+  } else {
+    p.peekError(t)
+    return false
+  }
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+  stmt := &ast.ReturnStatement{Token: p.curToken}
+
+  p.nextToken()
+
+  for !p.curTokenIs(token.SEMICOLON) {
+    p.nextToken()
+  }
+
+  return stmt
 }
