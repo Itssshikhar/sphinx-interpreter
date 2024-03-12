46513f9711c593c1f1df4626b83e45d13b64f541
Shikhar Mishra <shikhar2807.ace@gmail.com>
2024-03-12 23:28:00 +0530

    pratt parser added

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "sphinx/token"
+import (
+	"bytes"
+	"sphinx/token"
+)
 
 type Node interface {
   TokenLiteral() string
+  String() string
 }
 
 type Statement interface {
@@ -31,8 +35,18 @@ type ReturnStatement struct {
   ReturnValue Expression
 }
 
+type ExpressionStatement struct {
+  Token token.Token
+  Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+  return es.Token.Literal
+}
+
 func (rs *ReturnStatement) statementNode() {}
-func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
@@ -52,3 +66,54 @@ func (p *Program) TokenLiteral() string {
     return ""
   }
 }
+
+func (p *Program) String() string {
+  var out bytes.Buffer
+  
+  for _, s := range p.Statements {
+    out.WriteString(s.String())
+  }
+
+  return out.String()
+}
+
+func (ls *LetStatement) String() string {
+  var out bytes.Buffer
+
+  out.WriteString(ls.TokenLiteral() + " ")
+  out.WriteString(ls.Name.Value)
+  out.WriteString(" = ")
+
+  if ls.Value != nil {
+    out.WriteString(ls.Value.String())
+  }
+  
+  out.WriteString(";")
+
+  return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+  var out bytes.Buffer
+  
+  out.WriteString(rs.TokenLiteral() + " ")
+
+  if rs.ReturnValue != nil {
+    out.WriteString(rs.ReturnValue.String())
+  }
+
+  out.WriteString(";")
+  return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+  if es.Expression != nil {
+    return es.Expression.String()
+  }
+
+  return ""
+}
+
+func (i *Identifier) String() string { return i.Value }
+
+
