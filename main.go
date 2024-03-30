b8d38a90123ac7d776c5318505506091de98c1cf
Shikhar Mishra <shikhar2807.ace@gmail.com>
2024-03-30 13:57:44 +0530

    final REPL added

--- main.go
+++ main.go
@@ -8,10 +8,20 @@ import (
 )
 
 func main() {
+  const INTRO_LOGO = `           _     _            
+          | |   (_)           
+ ___ _ __ | |__  _ _ __ __  __
+/ __| '_ \| '_ \| | '_ \\ \/ /
+\__ \ |_) | | | | | | | |>  < 
+|___/ .__/|_| |_|_|_| |_/_/\_\
+    | |                       
+    |_|                       
+` 
   user, err := user.Current()
   if err != nil {
     panic(err)
   }
+  fmt.Print(INTRO_LOGO)
   fmt.Printf("Aa gya Manhoos %s! This is the Sphinx programming language!\n", user.Username)
   fmt.Printf("Feel free to type in commands\n")
   repl.Start(os.Stdin, os.Stdout)
