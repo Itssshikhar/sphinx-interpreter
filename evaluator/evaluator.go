90aba6dd8d36675f590b86de540730bd92fc605a
Shikhar Mishra <shikhar2807.ace@gmail.com>
2024-04-06 20:13:32 +0530

    infix exp evaluation added

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
   "sphinx/object"
 )
 
+var (
+  TRUE = &object.Boolean{Value: true}
+  FALSE = &object.Boolean{Value: false}
+  NULL = &object.Null{}
+)
+
 func Eval(node ast.Node) object.Object {
   switch node := node.(type) {
   case *ast.Program:
@@ -17,10 +23,26 @@ func Eval(node ast.Node) object.Object {
     return &object.Integer{Value: node.Value}
 
   case *ast.Boolean:
-    return &object.Boolean{Value: node.Value}
+    return nativeBooleanToBooleanObject(node.Value)
+
+  case *ast.PrefixExpression:
+    right := Eval(node.Right)
+    return evalPrefixExpression(node.Operator, right)
+  
+  case *ast.InfixExpression:
+    left := Eval(node.Left)
+    right := Eval(node.Right)
+    return evalInfixExpression(node.Operator, left, right)
   }
 
   return nil
+}
+
+func nativeBooleanToBooleanObject( input bool) *object.Boolean{
+  if input {
+    return TRUE
+  }
+  return FALSE
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
@@ -31,4 +53,72 @@ func evalStatements(stmts []ast.Statement) object.Object {
   }
 
   return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+  switch operator {
+  case "!":
+    return evalBangOperatorExpression(right)
+  case "-":
+    return evalMinusOperatorExpression(right)
+  default:
+    return NULL
+  }
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+  switch right {
+  case TRUE:
+    return FALSE
+  case FALSE:
+    return TRUE
+  case NULL:
+    return TRUE
+  default:
+    return FALSE
+  }
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+  if right.Type() != object.INTEGER_OBJ {
+    return NULL
+  }
+
+  value := right.(*object.Integer).Value
+  return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+  switch {
+  case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+    return evalIntegerInfixExpression(operator, left, right)
+  default:
+    return NULL
+  }
+}
+
+func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
+  leftVal := left.(*object.Integer).Value
+  rightVal := right.(*object.Integer).Value
+
+  switch operator {
+  case "+":
+    return &object.Integer{Value: leftVal + rightVal}
+  case "-":
+    return &object.Integer{Value: leftVal - rightVal}
+  case "*":
+    return &object.Integer{Value: leftVal * rightVal}
+  case "/":
+    return &object.Integer{Value: leftVal / rightVal}
+  case "<":
+    return nativeBooleanToBooleanObject(leftVal < rightVal)
+  case ">":
+    return nativeBooleanToBooleanObject(leftVal > rightVal)
+  case "==":  
+    return nativeBooleanToBooleanObject(leftVal == rightVal)
+  case "!=":
+    return nativeBooleanToBooleanObject(leftVal != rightVal)
+  default:
+    return NULL
+}
 }
